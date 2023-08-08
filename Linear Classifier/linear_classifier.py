a8595d74d1ac23dd2a8b7ecdf6f4c4004c1ada85
architJoshi2904 <aj6082@rit.edu>
2023-08-08 22:38:02 +0530

    Test and training data split

--- Linear Classifier/linear_classifier.py
+++ Linear Classifier/linear_classifier.py
@@ -5,8 +5,18 @@ Description : Implementing linear classifier
 numpy and matplotlib.
 Language : python3
 """
+
 import numpy as np
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import matplotlib
+
+SEED = 42
+
+
+def drawGraph(data):
+    # draw graph
+    pass
 
 
 def irisDataSet():
@@ -20,8 +30,27 @@ def irisDataSet():
     return data_table
 
 
+def trainLinearClassifier(dataset):
+    """
+    Dataset with only features to train linear classifier.
+
+    :param dataset: features without labels
+    :return: classifier
+    """
+    print("Inside Classifier")
+    print(dataset)
+
+
 def main():
+    # Data stack needs to be created with [features, labels]
     data = irisDataSet()
+    features, labels = data[:, :-1], data[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(features, labels,
+                                                        test_size=0.2,
+                                                        random_state=SEED)
+
+    # Train classifier
+
 
 
 if __name__ == "__main__":
